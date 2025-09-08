ea9a902d5a9b05853ef9e6b63b7d30839365fbb0
Seif Elnawawy <elnawawyseif@gmail.com>
2025-09-08 16:36:28 +0300

    finished education section

---
+++ lib/presentation/sections/educatino_section.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:seif_portofolio/core/constants/app_values.dart';
+import 'package:seif_portofolio/presentation/widgets/rounded_card.dart';
+
+class EducationSection extends StatelessWidget {
+  const EducationSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const RoundedCard(text: 'Education'),
+        const SizedBox(height: AppValues.paddingLarge),
+
+        Column(
+          children: const [
+            _EducationItem(
+              institution: "Ain Shams University",
+              degree: "Bachelor of Computer Science",
+              duration: "2023 – Present",
+              description:
+                  'Currently pursuing my degree with a focus on software engineering, mobile app development, Computer Architecture, Data Structures, Algorithms, OOP, Databases and more..',
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class _EducationItem extends StatelessWidget {
+  final String institution;
+  final String degree;
+  final String duration;
+  final String description;
+
+  const _EducationItem({
+    required this.institution,
+    required this.degree,
+    required this.duration,
+    required this.description,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(AppValues.paddingMedium),
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.primaryContainer,
+        borderRadius: BorderRadius.circular(AppValues.cardRadiusSmall),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            institution,
+            style: Theme.of(
+              context,
+            ).textTheme.titleMedium!.copyWith(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 8.0),
+          Text(
+            "$degree • $duration",
+            style: Theme.of(context).textTheme.labelLarge,
+          ),
+          const SizedBox(height: 12.0),
+          Text(description, style: Theme.of(context).textTheme.bodyMedium),
+        ],
+      ),
+    );
+  }
+}
--- lib/presentation/sections/home_screen.dart
+++ lib/presentation/sections/home_screen.dart
@@ -5,6 +5,7 @@ import 'package:seif_portofolio/core/constants/desktop_app_bar_actions.dart';
 import 'package:seif_portofolio/core/ui/responsive_layout.dart';
 import 'package:seif_portofolio/data/data_source/projects_data_source.dart';
 import 'package:seif_portofolio/data/data_source/skills_data_source.dart';
+import 'package:seif_portofolio/presentation/sections/educatino_section.dart';
 import 'package:seif_portofolio/presentation/sections/introduction_section/introduction_section_desktop.dart';
 import 'package:seif_portofolio/presentation/sections/introduction_section/introduction_section_mobile.dart';
 import 'package:seif_portofolio/presentation/sections/main_section/main_section_desktop.dart';
@@ -69,6 +70,12 @@ class HomeScreen extends StatelessWidget {
                 padding: _screenMainPadding(),
                 color: Theme.of(context).colorScheme.secondary,
                 child: WorkSection(projects: ProjectsDataSource.getProjects),
+              ),
+              Container(
+                width: double.infinity,
+                padding: _screenMainPadding(),
+                color: Theme.of(context).colorScheme.primary,
+                child: EducationSection(),
               ),
             ],
           ),
